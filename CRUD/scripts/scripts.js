1c424e950edd1c2b17af935eb7868a43019c43dd
paul <paulcj2001@hotmail.com>
2022-11-06 21:40:00 -0300

    second commit

--- CRUD/scripts/scripts.js
+++ CRUD/scripts/scripts.js
@@ -6,38 +6,85 @@ const results = document.getElementById('results');
 const inputPostNombre = document.getElementById('inputPostNombre');
 const inputPostApellido = document.getElementById('inputPostApellido');
 const btnPost = document.getElementById('btnPost');
+//const modal
+const modalInvalidFeedback = document.getElementById('modal-invalid-feedback');
 
-let getData = async () => {
+let getUsers = async () => {
     if(inputGetId1.value == 0 || ""){
         let responsive = await fetch('https://63651a5bf711cb49d1f52f76.mockapi.io/users');
-        let users = await responsive.json();
-        let htmlToAppend = "";
-        for( let user of users){
-            htmlToAppend += `<div class="ps-2">
-                                <li>ID: ${user.id}</li>
-                                <li>Nombre: ${user.name}</li>
-                                <li>Apellido: ${user.name}</li>    
-                                <hr>
-                            </div>
-                             `
+        if(responsive.status == 200){
+            let users = await responsive.json();
+            modalInvalidFeedback.classList.add('d-none');
+            modalInvalidFeedback.classList.remove('d-flex')
+            appendUsers(users)                 
+        }else {
+            modalInvalidFeedback.classList.add('d-flex');
+            modalInvalidFeedback.classList.remove('d-none')
         }
-        results.innerHTML = htmlToAppend;
     }else {
         let responsive = await fetch('https://63651a5bf711cb49d1f52f76.mockapi.io/users/'+inputGetId1.value)
-        let user = await responsive.json();
-        let htmlToAppend = "";
-        htmlToAppend += `<div class="ps-2">
-                            <li>ID: ${user.id}</li>
-                            <li>Nombre: ${user.name}</li>
-                             <li>Apellido: ${user.name}</li>    
-                             <hr>
-                        </div>`
-        results.innerHTML = htmlToAppend;
+        if(responsive.status == 200){
+            let user = await responsive.json();
+            console.log(user);
+            let users = [];
+            users[users.length] = user;
+            modalInvalidFeedback.classList.add('d-none');
+            modalInvalidFeedback.classList.remove('d-flex')
+            appendUsers(users);
+        }else{
+            modalInvalidFeedback.classList.add('d-flex');
+            modalInvalidFeedback.classList.remove('d-none')
+        }
+        
     };
 };
 
-let postUser = () => {
+let appendUsers = users => {
+    let htmlToAppend = "";
+    for ( let user of users ){
+        htmlToAppend += `<div class="ps-2">
+                                    <li>ID: ${user.id}</li>
+                                    <li>Nombre: ${user.name}</li>
+                                    <li>Apellido: ${user.name}</li>    
+                                    <hr>
+                                </div>
+                                `
+    };
+    results.innerHTML = htmlToAppend;
+};
 
+let postUser = async () => {
+    let url = "https://63651a5bf711cb49d1f52f76.mockapi.io/users";
+    let user = {
+        name: inputPostNombre.value,
+        lastname: inputPostApellido.value,
+        id: 10
+    }
+    let responsive = fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application.json' 
+        },
+        body: JSON.stringify(user)
+    }).then( res => res.json())
+    // let resultObj = await responsive.json();
+    // if(resultObj.status == 200){
+    //     modalInvalidFeedback.classList.add('d-none');
+    //     modalInvalidFeedback.classList.remove('d-flex')
+    // }else{
+    //     modalInvalidFeedback.classList.add('d-flex');
+    //     modalInvalidFeedback.classList.remove('d-none');
+    // }
 }
 
-btnGet1.addEventListener('click', getData);
+btnGet1.addEventListener('click', getUsers);
+btnPost.addEventListener('click', postUser);
+
+
+let checkInputReg = () => {
+    if (inputPostNombre.value!="" && inputPostApellido.value != ""){       
+        btnPost.disabled = false;
+    } else {
+        btnPost.disabled = true;
+    }
+}
