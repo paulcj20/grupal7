7255f88771c2da887bd9207bad91755d25cafb8b
paul <paulcj2001@hotmail.com>
2022-11-10 18:24:47 -0300

    commit

--- CRUD/scripts/scripts.js
+++ CRUD/scripts/scripts.js
@@ -14,20 +14,20 @@ const btnPut = document.getElementById('btnPut');
 
 let getUsers = async id => {
     if(id == 0 || ""){
-        let responsive = await fetch('https://63651a5bf711cb49d1f52f76.mockapi.io/users');
-        if(responsive.status == 200){
-            let users = await responsive.json();
+        let response = await fetch('https://63651a5bf711cb49d1f52f76.mockapi.io/users');
+        if(response.ok){
+            let users = await response.json();
             modalInvalidFeedback.classList.add('d-none');
             modalInvalidFeedback.classList.remove('d-flex')
             appendUsers(users)                 
         }else {
             modalInvalidFeedback.classList.add('d-flex');
-            modalInvalidFeedback.classList.remove('d-none')
+            modalInvalidFeedback.classList.remove('d-none');
         }
     }else {
-        let responsive = await fetch('https://63651a5bf711cb49d1f52f76.mockapi.io/users/'+inputGetId1.value)
-        if(responsive.status == 200){
-            let user = await responsive.json();
+        let response = await fetch('https://63651a5bf711cb49d1f52f76.mockapi.io/users/'+inputGetId1.value)
+        if(response.ok){
+            let user = await response.json();
             console.log(user);
             let users = [];
             users[users.length] = user;
@@ -48,7 +48,7 @@ let appendUsers = users => {
         htmlToAppend += `<div class="ps-2">
                                     <li>ID: ${user.id}</li>
                                     <li>Nombre: ${user.name}</li>
-                                    <li>Apellido: ${user.name}</li>    
+                                    <li>Apellido: ${user.lastname}</li>    
                                     <hr>
                                 </div>
                                 `
@@ -58,25 +58,30 @@ let appendUsers = users => {
 
 let postUser = async () => {
     let url = "https://63651a5bf711cb49d1f52f76.mockapi.io/users";
-    let usuario = {
-        'name': inputPostNombre.value,
-        'lastname': inputPostApellido.value,
-       
-    }
-    let responsive = await fetch(url, {
+    let usuario = {'name': inputPostNombre.value,'lastname': inputPostApellido.value}
+    let response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json' 
         },
         body: JSON.stringify(usuario)
-    }).then( res => res.json()).catch(errror => {
-        
-    })
-    getUsers(0)
+    });
+    if(response.ok){
+        getUsers(0);
+        modalInvalidFeedback.classList.add('d-none');
+        modalInvalidFeedback.classList.remove('d-flex');
+        let result = await response.json();
+        console.log(result);
+
+    }else{
+        modalInvalidFeedback.classList.add('d-flex');
+        modalInvalidFeedback.classList.remove('d-none');
+    }
+    
 }
 
 let putUser = async () => {
-
+    
 };
 
 btnGet1.addEventListener('click', () => getUsers(inputGetId1.value));
